6ca27979c13caf94dfbc1f0c6d26d23280107d9d
alright <2533713839@qq.com>
2021-05-20 00:34:55 +0800

    解决了跨域问题

--- src/utils/request.js
+++ src/utils/request.js
@@ -16,12 +16,13 @@ service.interceptors.request.use(
   config => {
     // do something before request is sent
 
-    if (store.getters.token) {
-      // let each request carry token
-      // ['X-Token'] is a custom headers key
-      // please modify it according to the actual situation
-      config.headers['X-Token'] = getToken()
-    }
+    // if (store.getters.token) {
+    //   // let each request carry token
+    //   // ['X-Token'] is a custom headers key
+    //   // please modify it according to the actual situation
+    //   config.headers['X-Token'] = getToken()
+    // }
+    // config.headers['Origin'] = 'https://vignetting.work'
     return config
   },
   error => {
